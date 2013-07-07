3a9c176964f7d46006f88a5b1cf11138b66d3e85
Sia K <siakaramalegos@gmail.com>
2013-07-07 20:02:28 +0000

    changed web.js code to pull from index.html

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+//var buffer= new Buffer('');
+var buffer=fs.readFileSync('index.html', 'utf8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  response.send(buffer);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {

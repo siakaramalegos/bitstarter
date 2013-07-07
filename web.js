8e4e316cafb468be7047d1c8e3287b7f2e865ef7
Sia K <siakaramalegos@gmail.com>
2013-07-07 21:14:47 +0000

    trying again to fix web.js

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-//var buffer= new buffer();
-//var fs=require('fs');
-//var buffer=fs.readFileSync('index.html', 'utf8');
+var fs=require('fs');
+var buff=new buffer(fs.readFileSync('index.html', 'utf8'));
 
 app.get('/', function(request, response) {
-  var buffer= new buffer();
-  var fs=require('fs');
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
+//  var buffer= new buffer();
+//  var fs=require('fs');
+  response.send(buff.toString());
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {

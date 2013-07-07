7df6494f92b0b1f62354f2c3d67267b889fd9d98
Sia K <siakaramalegos@gmail.com>
2013-07-07 21:21:47 +0000

    one more time web.js

--- web.js
+++ web.js
@@ -8,6 +8,7 @@ app.get('/', function(request, response) {
 //  var buffer= new buffer();
 //  var fs=require('fs');
   response.send(buff.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
